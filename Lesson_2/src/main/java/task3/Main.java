d65288406a3d92b8c91e6ce89f0c9179d317de53
Zero-imagination <disagreed7@gmail.com>
2022-09-13 01:51:20 +0300

    added braces for if else methods

--- Lesson_2/src/main/java/task3/Main.java
+++ Lesson_2/src/main/java/task3/Main.java
@@ -12,21 +12,28 @@ public class Main {
             {"lw", "cartwheel"}, // false
             {null, "car"} // false
     };
+
     public static void main(String[] args) {
-        if (Objects.nonNull(inputData))
-            for (String[] input: inputData) System.out.println(fuzzySearch(input[0],input[1]));
+        if (Objects.nonNull(inputData)) {
+            for (String[] input : inputData) System.out.println(fuzzySearch(input[0], input[1]));
+        }
     }
+
     public static boolean fuzzySearch(String target, String stringData){
-        if (!Objects.nonNull(target)||!Objects.nonNull(stringData)) return false;
+        if (!Objects.nonNull(target)||!Objects.nonNull(stringData)){
+            return false;
+        }
         String[] charsTarget=target.split("");
         int prevPosition = 0;
         StringBuilder localStringData = new StringBuilder(stringData);
         for (String character: charsTarget) {
-            if(localStringData.toString().contains(character) &&
-                    stringData.indexOf(character)>=prevPosition){
+            if (localStringData.toString().contains(character)
+                    && stringData.indexOf(character)>=prevPosition){
                 localStringData.deleteCharAt(localStringData.indexOf(character));
                 prevPosition=stringData.indexOf(character);
-            } else return false;
+            } else {
+                return false;
+            }
         }
         return true;
     }
